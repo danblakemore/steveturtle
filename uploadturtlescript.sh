5e00aad769067434132be03be5e0783572c855b2
Daniel Blakemore <danblakemore@gmail.com>
2013-06-29 16:48:41 -0600

    added move API upload to script for mining

--- uploadturtlescript.sh
+++ uploadturtlescript.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 scp "spoork.lua" trevor:
+scp "move.lua" trevor:
 ssh trevor "cp ~/spoork.lua /home/trevor/Minecraft/FTB-Beta-A/computer/0/minediamond"
+ssh trevor "cp ~/move.lua /home/trevor/Minecraft/FTB-Beta-A/computer/0/move"
